ac9957f7a802c90407ce44d49b5284824de893ac
David Saidov <delightxxls@gmail.com>
2025-04-18 18:39:37 +0300

    chore: add ALLOWED_ORIGINS

--- src/main.py
+++ src/main.py
@@ -22,8 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(user_router_v1, prefix='/api/v1', )

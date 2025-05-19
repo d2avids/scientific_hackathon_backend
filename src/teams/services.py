5aed26188ce4e8bf84d4722b308a2c060a8227a4
Aleksander <therealchistyakov@ya.ru>
2025-05-19 21:49:20 +0300

    chore: add team already exists raising

--- src/teams/services.py
+++ src/teams/services.py
@@ -28,6 +28,11 @@ class TeamService:
                 status_code=status.HTTP_409_CONFLICT,
                 detail='Integrity error. Check the data for correctness.'
             )
+        except AlreadyExistsError:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Team with this name already exists'
+            )
 
     async def update_team(
             self,
